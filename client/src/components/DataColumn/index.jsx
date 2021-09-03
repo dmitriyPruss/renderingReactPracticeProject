58a10ebc7f8c4c6745e42c2a4bab7518c30d2a81
Dmitriy <dmitriypruss21041984@gmail.com>
2021-09-03 03:13:31 +0300

    add hooks useState and useEffect

--- client/src/components/DataColumn/index.jsx
+++ client/src/components/DataColumn/index.jsx
@@ -1,35 +1,59 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 
-class DataColumn extends Component {
-  constructor (props) {
-    super(props);
+// class DataColumn extends Component {
+//   constructor (props) {
+//     super(props);
 
-    this.state = {
-      data: [],
-      isFetching: false,
-      error: null,
-    };
-  }
+//     this.state = {
+//       data: [],
+//       isFetching: false,
+//       error: null,
+//     };
+//   }
 
-  componentDidMount () {
-    this.setState({ isFetching: true });
-    fetch(this.props.fileName)
+//   // Переписать на хуки
+//   loadData = () => {
+//     this.setState({ isFetching: true });
+//     fetch(this.props.fileName)
+//       .then(res => res.json())
+//       .then(data => this.setState({ data }))
+//       .catch(error => this.setState({ error }))
+//       .finally(() => {
+//         this.setState({ isFetching: false });
+//       });
+//   };
+
+//   componentDidMount () {
+//     this.loadData();
+//   }
+
+//   render = () => this.props.render(this.state);
+// }
+
+function DataColumn (props) {
+  const [data, setData] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState(null);
+
+  console.log('props :>> ', props);
+
+  const loadData = () => {
+    setIsFetching(true);
+
+    fetch(props.fileName)
       .then(res => res.json())
-      .then(data => this.setState({ data }))
-      .catch(error => this.setState({ error }))
+      .then(data => setData(data))
+      .catch(error => setError(error))
       .finally(() => {
-        this.setState({ isFetching: false });
+        setIsFetching(false);
       });
-  }
+  };
 
-  render () {
-    const { render } = this.props;
+  useEffect(() => {
+    loadData();
+  }, []);
 
-    console.log('render :>> ', { ...this.state });
-    const layout = render({ ...this.state });
-
-    return layout;
-  }
+  return props.render({ data, isFetching, error });
 }
 
 export default DataColumn;
