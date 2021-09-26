4e49fd857bf2e66767a6343be2df570e981a56ba
Dmitriy <dmitriypruss21041984@gmail.com>
2021-09-26 13:04:08 +0300

    add Hooks

--- client/src/pages/PricingPage/PricesList/PricesListItem/index.jsx
+++ client/src/pages/PricingPage/PricesList/PricesListItem/index.jsx
@@ -1,178 +1,65 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import PricesElementHeader from './PricesElementHeader';
+import ListElements from './ListElements';
 import styles from './../PricesList.module.sass';
 
-class PricesListItem extends Component {
-  constructor (props) {
-    super(props);
+function PricesListItem (props) {
+  const [toggleSign, setToggleSign] = useState(true);
 
-    this.state = {
-      toggleSign: true,
-    };
+  const listRef = useRef(null);
 
-    this.listRef = React.createRef();
-  }
+  const toggleElement = () => {
+    listRef.current.style.display === 'block'
+      ? (listRef.current.style.display = 'none')
+      : (listRef.current.style.display = 'block');
 
-  toggleElement = () => {
-    this.listRef.current.style.display === 'block'
-      ? (this.listRef.current.style.display = 'none')
-      : (this.listRef.current.style.display = 'block');
-
-    this.state.toggleSign
-      ? this.setState({ toggleSign: false })
-      : this.setState({ toggleSign: true });
+    toggleSign ? setToggleSign(false) : setToggleSign(true);
   };
 
-  render () {
-    const {
-      priceItem: {
-        colorName,
-        description,
-        price: { amount, currency },
-        options,
-      },
-      color,
-    } = this.props;
+  const {
+    priceItem: {
+      colorName,
+      description,
+      price: { amount, currency },
+      options,
+    },
+    color,
+  } = props;
 
-    const { toggleSign } = this.state;
-
-    return (
-      <li className={styles.pricesListItem}>
-        <PricesElementHeader
-          classHeader={styles.header}
-          toggleSign={toggleSign}
-          colorName={colorName}
-          description={description}
-          toggledButton={styles.toggledButton}
-          toggleElement={this.toggleElement}
-          currencyValuesClass={styles.currency}
-          styleColor={{ color: color }}
-          currency={currency}
-          amount={amount}
-          currencyValueClass={styles.currencyValue}
+  return (
+    <li className={styles.pricesListItem}>
+      <PricesElementHeader
+        classHeader={styles.header}
+        toggleSign={toggleSign}
+        colorName={colorName}
+        description={description}
+        toggledButton={styles.toggledButton}
+        toggleElement={toggleElement}
+        currencyValuesClass={styles.currency}
+        styleColor={{ color: color }}
+        currency={currency}
+        amount={amount}
+        currencyValueClass={styles.currencyValue}
+      />
+      <section className={styles.mainInfo} ref={listRef}>
+        <ListElements
+          options={options}
+          optionsListClass={styles.optionsList}
+          showedElemClass={styles.showedElem}
+          innerShowedElementClass={styles.innerShowedElement}
+          unshowedElemClass={styles.unshowedElem}
         />
-        <section className={styles.mainInfo} ref={this.listRef}>
-          <ul className={styles.optionsList}>
-            {options.map((o, i) => {
-              if (Array.isArray(o)) {
-                return (
-                  <li key={i}>
-                    {o.map((item, index) => {
-                      if (Array.isArray(item)) {
-                        return (
-                          <div>
-                            {item.map(innerItem => {
-                              const linkStr = 'View Sample Report';
-
-                              const iWithoutSpaces = innerItem.text
-                                .split(' ')
-                                .join('');
-
-                              if (
-                                iWithoutSpaces.indexOf(
-                                  linkStr.split(' ').join('')
-                                ) !== -1
-                              ) {
-                                let mainStr = '';
-                                const mainStrArr = innerItem.text.split(' ');
-
-                                for (let i = 0; i < mainStrArr.length; i++) {
-                                  if (
-                                    mainStrArr[i] !== 'View' &&
-                                    mainStrArr[i] !== 'Sample' &&
-                                    mainStrArr[i] !== 'Report'
-                                  ) {
-                                    mainStr += mainStrArr[i] + ' ';
-                                  }
-                                }
-
-                                return (
-                                  <p
-                                    className={`${styles.showedElem} ${styles.innerShowedElement}`}
-                                    data-tooltip={innerItem.tooltip}
-                                  >
-                                    <i
-                                      style={{ marginRight: '5px' }}
-                                      className='fas fa-check'
-                                    />
-                                    <span>{mainStr}</span>
-                                    <br />
-                                    <a
-                                      style={{ textDecoration: 'none' }}
-                                      href='https://www.squadhelp.com/assets/nimages/AudienceTestingReport.pdf'
-                                    >
-                                      {linkStr}
-                                    </a>
-                                  </p>
-                                );
-                              }
-                              return (
-                                <p
-                                  className={`${styles.showedElem} ${styles.innerShowedElement}`}
-                                  data-tooltip={innerItem.tooltip}
-                                >
-                                  <i
-                                    style={{ marginRight: '5px' }}
-                                    className='fas fa-check'
-                                  />
-                                  <span>{innerItem.text}</span>
-                                </p>
-                              );
-                            })}
-                          </div>
-                        );
-                      } else {
-                        return (
-                          <div
-                            className={styles.showedElem}
-                            data-tooltip={item['tooltip']}
-                          >
-                            {item['text']}
-                          </div>
-                        );
-                      }
-                    })}
-                  </li>
-                );
-              } else if (typeof o === 'string') {
-                return o.indexOf('Learn') === 0 ? (
-                  <p className={styles.unshowedElem}>
-                    {o.split('|')[0]}
-                    <a
-                      style={{ textDecoration: 'none' }}
-                      href='https://www.squadhelp.com/managed-contests'
-                    >
-                      {o.split('|')[1]}
-                    </a>
-                  </p>
-                ) : (
-                  <p className={styles.unshowedElem}>{o}</p>
-                );
-              } else {
-                return (
-                  <li
-                    className={styles.showedElem}
-                    data-tooltip={o['tooltip']}
-                    key={i}
-                  >
-                    {o['text']}
-                  </li>
-                );
-              }
-            })}
-          </ul>
-          <Link
-            to='#'
-            style={{ backgroundColor: color }}
-            className={styles.startLink}
-          >
-            <i className='fas fa-check' /> Start
-          </Link>
-        </section>
-      </li>
-    );
-  }
+        <Link
+          to='#'
+          style={{ backgroundColor: color }}
+          className={styles.startLink}
+        >
+          <i className='fas fa-check' /> Start
+        </Link>
+      </section>
+    </li>
+  );
 }
 
 export default PricesListItem;
